05e1c042e977789e158c7451442b5b81bb78c386
jupp-ag <jeffersonrhoneystv2@gmail.com>
2024-07-10 12:56:54 -0700

    Delete stake.lido directory

--- stake.lido/_next/static/chunks/pages/rewards-c77d4eb5c3e4dd88.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{62197:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/rewards",function(){return u(95341)}])}},function(n){n.O(0,[774,888,179],(function(){return _=62197,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
